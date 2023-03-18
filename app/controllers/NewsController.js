02176423cdd8807fbe906048b90253cd30ad24cb
tinydead <91645249+tinydead@users.noreply.github.com>
2023-03-18 22:40:51 +0700

    Add new features for update and delete news by id

--- app/controllers/NewsController.js
+++ app/controllers/NewsController.js
@@ -1,13 +1,11 @@
 const News = require('../models/News')
-const { mongooseToObject, mutipleMongooseToObject } = require('../../util/mongoose')
 
 class NewsController {
+
     //[GET] /news
     getAll(req, res, next) {
         News.find({})
-            .then((news) => 
-                res.json(mutipleMongooseToObject(news)
-                ))
+            .then( news => res.json(news))
             .catch(next);
     }
 
@@ -19,6 +17,26 @@ class NewsController {
             .then(() => res.send('Add successfully'))
             .catch(next);
 
+    }
+
+    //[GET] /news/:id
+    getById(req, res, next) {
+        News.findById(req.params.id)
+            .then((course) => res.json(course))
+    }
+
+    //[PUT] /news/:id
+    update(req, res, next) {
+        News.updateOne({_id: req.params.id}, req.body)
+            .then(() => res.redirect('/news/' + req.params.id))
+            .catch(next);
+    }
+
+    //[DELETE] /news/:id
+    destroy(req, res, next) {
+        News.deleteOne({_id: req.params.id})
+            .then(() => res.redirect('/news/'))
+            .catch(next);
     }
 } 
 
--- routes/news.js
+++ routes/news.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const newsController = require('../app/controllers/NewsController');
 
+router.get('/:id', newsController.getById);
+router.put('/:id', newsController.update);
+router.delete('/:id', newsController.destroy);
 router.get('/', newsController.getAll);
 router.post('/store', newsController.store);
 
